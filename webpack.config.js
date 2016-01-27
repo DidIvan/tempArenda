cd33e5b82a61cd5b433cd0a9b7c2b8e168fe641c
Ivan <Holy.vinil@gmail.com>
2016-01-27 18:06:41 +0200

    ы

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,23 @@
 const NODE_ENV = process.env.NODE_ENV || "development";
 
 module.exports = {
-    entry: "./public/js/components/markup/home.js",
-    output: {
-        filename: "./packaged/build.js",
-        library:"home"
+    entry: {
+        /*точки входа*/
+        header: "./public/js/components/markup/header-id.js",
+        bestGoods: "./public/js/components/markup/bestGoods.js"
     },
+    output: {
+        /*абсолютный путь к директории сборки*/
+        path:__dirname+'/packaged',
+        filename: "[name].js",
+        library: "[name]"
+    },
+    /*   entry: "./public/js/components/markup/home.js",
+     output: {
+     filename: "./packaged/build.js",
+     /!*експорт модуля будет помещен в переменную с таким названием*!/
+     library:"home"
+     },*/
     watch: NODE_ENV == "development",
     /*параметр-задает время ожидания после изменения в течение которого не запускается сборка*/
     watchOptions: {
@@ -17,5 +29,12 @@ module.exports = {
     },
     /*разделяет запакованый файл на отдельные модули*/
     /*   devtool: "source-map"   */
-    devtool: NODE_ENV == "development" ? "cheap-module-eval-source-map" : null
+    devtool: NODE_ENV == "development" ? "cheap-module-eval-source-map" : null,
+
+    module: {
+        loaders: [
+            { test: /\.js$/, loader:'jsx-loader'}
+            // => "jade" loader is used for ".jade" files
+        ]
+    }
 };
